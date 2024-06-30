bb87d9ed716634492b3e1d177b8f7781c447409d
Alexandr_Buranov <db9vol42rus@mail.ru>
2024-07-01 00:08:06 +0700

    widget.py - interpritate_2

--- src/widget.py
+++ src/widget.py
@@ -4,8 +4,7 @@ from src.masks import get_mask_card_number, get_mask_account
 def mask_account_card(number: str) -> str:
     """Функция получает строку и москирует номер карты или счёта"""
     if len(number.split()[-1]) == 16:
-        new_number = get_mask_card_number(number.split()[-1])
-        result = f'{number[:-16]}{new_number}'
+        result = f'{number[:-16]}{get_mask_card_number(number.split()[-1])}'
         return result
     elif len(number.split()[-1]) == 20:
         result = f'{number[:-20]}{get_mask_account(number.split()[-1])}'
