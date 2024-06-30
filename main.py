3912fcc744729a2a86a87cc17b8ee3599f62e745
Alexandr_Buranov <db9vol42rus@mail.ru>
2024-07-01 00:03:36 +0700

    Dz_9.1, Dz_9.2

---
+++ main.py
@@ -0,0 +1,5 @@
+from src.widget import mask_account_card, get_data
+
+print(mask_account_card('Visa Platinum 8990922113665229'))
+print(mask_account_card('Счет 64686473678894779589'))
+print(get_data('2018-07-11T02:26:18.671407'))
---
+++ src/widget.py
@@ -0,0 +1,19 @@
+from src.masks import get_mask_card_number, get_mask_account
+
+
+def mask_account_card(number: str) -> str:
+    """Функция получает строку и москирует номер карты или счёта"""
+    if len(number.split()[-1]) == 16:
+        new_number = get_mask_card_number(number.split()[-1])
+        result = f'{number[:-16]}{new_number}'
+        return result
+    elif len(number.split()[-1]) == 20:
+        new_number = get_mask_account(number.split()[-1])
+        result = f'{number[:-20]}{new_number}'
+        return result
+
+
+def get_data(data_string: str) -> str:
+    """Функция принимает дату и меняет формат"""
+    data_slise = data_string[0:10].split('-')
+    return '.'.join(data_slise[::-1])
