d9d200d288f4f0d304aaa05a1f34ad8be32dc419
Alexandr_Buranov <db9vol42rus@mail.ru>
2024-07-05 01:23:23 +0700

    feature work filter_by_state

--- src/processing.py
+++ src/processing.py
@@ -1,6 +1,24 @@
-def filter_by_state():
-    pass
+state_list = [
+    {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+    {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
+]
+
+
+def filter_by_state(state_list: list[dict[str]], state_id="EXECUTED") -> list[dict[str]]:
+
+    list_state = []
+
+    for key in state_list:
+        if key.get("state") == state_id:
+            list_state.append(key)
+
+    return list_state
 
 
 def sort_by_date():
     pass
+
+print(filter_by_state(state_list, state_id="EXECUTED"))
+print(filter_by_state(state_list, state_id="CANCELED"))
